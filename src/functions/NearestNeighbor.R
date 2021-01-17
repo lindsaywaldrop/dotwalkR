99e7ffb56d6e0725942009b4a71da27489b3ce70
lindsaywaldrop <waldrop@chapman.edu>
2021-01-17 12:25:57 -0800

    Working version of NN, now getMeanValues, optimized for speed and memory

--- src/functions/NearestNeighbor.R
+++ src/functions/NearestNeighbor.R
@@ -30,3 +30,23 @@ GetNeighbors <- function(vectors, testVector, desiredNeighbors) {
   }
   return(neighbors)
 }
+
+getMeanValue <- function(vectors, values, testVector, desiredNeighbors) {
+  require(data.table)
+  means<-rep(NA, length=dim(testVector)[1])
+  for (i in 1:dim(testVector)[1]) {
+    distances<-matrix(data=NA, nrow=dim(vectors)[1], ncol=2)
+    colnames(distances)<-c("index","distance")
+    distances[,1]<-seq(1:dim(vectors)[1])
+    distances[,2]<-sqrt((vectors[, 1] - testVector[i, 1])^2 +
+                        (vectors[, 2] - testVector[i, 2])^2 + 
+                        (vectors[, 3] - testVector[i, 3])^2)
+    distances2 <- data.table(distances)
+    sorted.distances <- setkey(distances2, "distance")
+    final.dist<- sorted.distances[1:desiredNeighbors,]
+    final.dist$wgts <- 1 - 20*(sorted.distances[1:desiredNeighbors,2])
+    final.dist$value <- values[final.dist$index]
+    means[i] <- weighted.mean(as.numeric(final.dist$value), final.dist$wgts)
+  }
+  return(means)
+}
--- src/loadMatData.R
+++ src/loadMatData.R
@@ -2,6 +2,7 @@
 library(R.matlab)
 
 source("./src/functions/NearestNeighbor.R")
+source("./src/functions/reshape_functions.R")
 
 # These are the 3D coordinates of the surrogate plot.
 input.data <- readMat("./test/exampledata/input_resampled.mat")
@@ -15,44 +16,35 @@ fmat.data <- readMat("./test/exampledata/clcd_resampled_long.mat")
 # coordinates in input.data[["input"]].
 surrogate<-fmat.data[["fmat"]]
 
-test.dot<-c(10.11, 0.19866, 117500)
-test.dot<-matrix(data=NA, nrow = 10, ncol = 3)
-test.dot[1,] <-  c(10.11, 0.19866, 117500)
-test.dot[2,] <- c(7.98, 0.09866, 107534)
-test.dot[3,] <- c(4.11, 0.166, 75000)
-test.dot[4,] <- c(4.11, 0.166, 75000)
-test.dot[5,] <- c(4.11, 0.166, 75000)
-test.dot[6,] <- c(4.11, 0.166, 75000)
-test.dot[7,] <- c(4.11, 0.166, 75000)
-test.dot[8,] <- c(4.11, 0.166, 75000)
-test.dot[9,] <- c(4.11, 0.166, 75000)
-test.dot[10,] <- c(4.11, 0.166, 75000)
+n<-100
+test.dot<-matrix(data=NA, nrow = n, ncol = 3)
+test.dot[,1] <-
+  runif(n,
+        min = min(input.data[["input"]][, 1]),
+        max = max(input.data[["input"]][, 1]))
+test.dot[,2] <-
+  runif(n,
+        min = min(input.data[["input"]][, 2]),
+        max = max(input.data[["input"]][, 2]))
+test.dot[,3] <-
+  runif(n,
+        min = min(input.data[["input"]][, 3]),
+        max = max(input.data[["input"]][, 3]))
 
 sample.dot<-scale.dots(test.dot, 
                        range(input.real[,1]), range(input.real[,2]), range(input.real[,3]))
-idxs<-list()
-mean.ps<-rep(NA, length=10)
-for (k in 1:length(sample.dot)){
-  idxs <- GetNeighbors(input.scaled, sample.dot[[k]], 3)
-  idxs.df <- data.frame(input.real[idxs[[1]]$index,], 
-                             input.real[idxs[[2]]$index,], 
-                             input.real[idxs[[3]]$index,])
-  values.df <- data.frame(surrogate[idxs[[1]]$index], 
-                          surrogate[idxs[[2]]$index], 
-                          surrogate[idxs[[3]]$index])
-  wgts <- 1 - 20*c(idxs[[1]]$distance,idxs[[2]]$distance, idxs[[3]]$distance)
-  mean.ps[k] <- weighted.mean(as.numeric(values.df), wgts)
-}
+##NN 1
+#idxs<-list()
+#mean.ps<-rep(NA, length=n)
+#for (k in 1:length(sample.dot)){
+#  idxs <- GetNeighbors(input.scaled, sample.dot[[k]], 3)
+#  values<-rep(NA,3)
+#  for (u in 1:3) values[u]<-surrogate[idxs[[u]]$index]
+#  wgts <- 1 - 20*c(idxs[[1]]$distance, idxs[[2]]$distance, idxs[[3]]$distance)
+#  mean.ps[k] <- weighted.mean(as.numeric(values), wgts)
+#}
 
+mean.ps<-rep(NA, length=n)
+idxs<-getMeanValue(input.scaled, surrogate, sample.dot, 3)
 
-
-plot(input.real[,1],input.real[,2])
-points(test.dot[1],test.dot[2],pch=19,col="blue")
-points(idxs.df[1,],idxs.df[2,],pch=19,col="red")
-print(values.df)
-
-
-mean.ps <- weighted.mean(as.numeric(values.df), wgts)
-resid.ps <- mean.ps - as.numeric(values.df)
-  
   
